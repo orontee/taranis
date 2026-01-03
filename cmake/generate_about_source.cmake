241aa632c8382caf7775039b5a81d8a69a5172f9
Matthias Meulien <orontee@gmail.com>
2026-01-03 19:10:20 +0100

    Fix possible wrong version number in devel build

--- cmake/generate_about_source.cmake
+++ cmake/generate_about_source.cmake
@@ -3,12 +3,20 @@
 find_package(Git REQUIRED)
 execute_process(
   COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
-  OUTPUT_VARIABLE VCS_TAG
+  OUTPUT_VARIABLE VCS_SHORT_COMMIT
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-if(NOT VCS_TAG)
-  set(VCS_TAG "devel")
+execute_process(
+  COMMAND ${GIT_EXECUTABLE} describe --tags --match "v[0-9]*.[0-9]*.[0-9]*"
+  OUTPUT_VARIABLE VCS_DESCRIBE_COMMIT
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+if(VCS_DESCRIBE_COMMIT STREQUAL "v${taranis_VERSION}")
+  set(ABOUT_PROJECT_VERSION "${taranis_VERSION} (${VCS_SHORT_COMMIT})")
+else()
+  set(ABOUT_PROJECT_VERSION "${taranis_VERSION}-devel (${VCS_DESCRIBE_COMMIT})")
 endif()
 
 configure_file(
@@ -16,6 +24,10 @@ configure_file(
   about.cc
   @ONLY
 )
+
+unset(ABOUT_PROJECT_VERSION)
+unset(VCS_DESCRIBE_COMMIT)
+unset(VCS_SHORT_COMMIT)
 
 list(APPEND ABOUT_SOURCES
   ${CMAKE_CURRENT_BINARY_DIR}/about.cc
